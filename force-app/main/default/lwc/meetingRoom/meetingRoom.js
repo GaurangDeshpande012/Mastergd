1f8202ffbf17dc63d364da6c7fb304059d306155
Gaurang Deshpande <gauranggdeshpande@gmail.com>
2020-05-16 23:17:38 +0530

    commit

---
+++ force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -0,0 +1,15 @@
+import { LightningElement ,api  } from 'lwc';
+
+export default class MeetingRoom extends LightningElement {
+@api meetingRoomInfo;  //{roomName: 'A -01',roomCapacity:'12'}
+
+
+
+
+
+
+
+
+
+
+}
---
+++ force-app/main/default/lwc/meetingRooms/meetingRooms.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+export default class MeetingRooms extends LightningElement {
+
+meetingRoomsInfo=[          
+
+    {roomName: 'A -01',roomCapacity:'12'},
+    {roomName: 'A -02',roomCapacity:'13'},
+    {roomName: 'A -03',roomCapacity:'16'},
+    {roomName: 'B -01',roomCapacity:'25'},
+    {roomName: 'B -02',roomCapacity:'20'},
+    {roomName: 'C -01',roomCapacity:'15'},
+    {roomName: 'C -02',roomCapacity:'10'}
+];
+
+
+
+}
